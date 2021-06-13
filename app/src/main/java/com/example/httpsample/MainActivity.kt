b8304f1dee2828756312edf09dec6a745d978d3b
hirokuma <hiro9ma@gmail.com>
2021-06-13 22:59:54 +0900

    suspend funでないなら動く

--- app/src/main/java/com/example/httpsample/MainActivity.kt
+++ app/src/main/java/com/example/httpsample/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.httpsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.ResponseBody
-import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     // https://api.github.com/users/hirokuma
@@ -25,21 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     interface UserService {
         @GET("/users/{id}")
-        fun getRawResponseForPosts(@Path("id") id: String): Call<ResponseBody>
-
-        @GET("/users/{id}")
-        suspend fun getUser(@Path("id") id: String): User
+        fun getUser(@Path("id") id: String): Call<User>
     }
-
-    class UserRepository constructor(
-        private val userService: UserService
-    ) {
-        suspend fun getUserById(id: String): User {
-            return userService.getUser(id)
-        }
-    }
-
-    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,22 +32,13 @@ class MainActivity : AppCompatActivity() {
         Thread(Runnable {
             val retrofit = Retrofit.Builder().apply {
                 baseUrl("https://api.github.com/")
+                addConverterFactory(GsonConverterFactory.create())
             }.build()
 
             val service = retrofit.create(UserService::class.java)
-            val repos = UserRepository(service)
-
-            scope.launch {
-                // コルーチンとかsuspend functionからしか呼べない
-                val koko = service.getRawResponseForPosts("hirokuma")
-                val responseBody = koko.execute()
-                responseBody.body()?.let {
-                    Log.d("launch", it.string())
-                }
-//                val user = repos.getUserById("hirokuma")
-//                Log.d("launch", user.toString());
-            }
+            val call = service.getUser("hirokuma")
+            val res = call.execute().body()
+            Log.d("launch", res.toString())
         }).start()
-
     }
 }
